48fb6ebb7694054054aaad2c581176fc46ced527
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-09 17:26:27 +0100

    Update inital_test.py

--- inital_test.py
+++ inital_test.py
@@ -1,17 +1,20 @@
 import pymongo
 import pandas as pd
 
-csv = pd.read_csv("")
+csv = pd.read_csv("C:/Users/Administrator/Code/rusty_snapshots/snapshots/2024-05-09_17-00/CoinM.csv").to_dict(orient='records')
+
+
 
 mongo_uri = "mongodb://localhost:27017/"
 
 client = pymongo.MongoClient(mongo_uri)
-db = client["test"]
+db = client["test_csv"]
 
-collection_name = "test_collection_2"
+collection_name = "2024-05-09_17-00_CoinM"
 collection = db[collection_name]
 
-result = collection.insert_one({"key": "value_2"})  # Example document (key-value pair)
+for i in csv:
+    result = collection.insert_one(i)  # Example document (key-value pair)
 
 # Check if the collection was created successfully
 if result.acknowledged:
@@ -24,7 +27,7 @@ print(db.list_collection_names())
 try:
     # Connect to MongoDB
     client = pymongo.MongoClient(mongo_uri)
-    db = client["test"]
+    db = client["test_csv"]
 
     # Access the specified collection
     collection = db[collection_name]
