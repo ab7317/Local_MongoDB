c57897f65b95e04abbeece3852dee7bd9bc4fd0e
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 16:07:14 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -10,12 +10,17 @@ Client = mc.MongoDBClient("mongodb://localhost:27017/", "test_new")
 
 #print(Client.get_collections())
 
-print(Client.get_collection_content("CoinM"), "\n##########################")
+#print(Client.get_collection_content("CoinM"), "\n##########################")
 
-Client.query([{"Coin":"ATOM"}, {"Lever": {"$gt": 3}}], "CoinM")
+result = Client.query([{"Coin":"ATOM"}, {"USDValue": {"$gt": 1000000}}, {"Date": {"$lt":"2024-05-10"}}], "FB_Assets")
+
+print(result)
+
+sum_of_atom = result['USDValue'].sum()
+
+print(sum_of_atom)
 
 csv_files = Client.build_csv_path_list("C:/Users/Administrator/Code/rusty_snapshots/snapshots")
 
 for csv in csv_files:
     Client.insert_csv_collection(csv.split("/")[-1][:-4], csv)
-
