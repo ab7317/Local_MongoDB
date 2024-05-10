febdfb3e18cb847bb4fc04f87338e5f188c2ef58
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 10:23:12 +0100

    Update DB_Class.py

--- DB_Class.py
+++ DB_Class.py
@@ -1,4 +1,4 @@
-import pymongo
+import pymongo, os
 import pandas as pd
 
 class MongoDBClient:
@@ -28,3 +28,19 @@ class MongoDBClient:
         {field: {"$exists": True} for field in cols_to_query},
         {field: 1 for field in cols_to_query + ["_id"]}
         )))
+
+    def build_csv_path_list(self, csv_dir_path):#
+        ###
+        #This function will build a list only of csv files it will ignore any
+        #other file types inside the directories
+        #It will also only work on a single child directory 
+        ###
+
+        full_path_list = []
+        dir_list = os.listdir(csv_dir_path)
+        for directory in dir_list:
+            file_list = os.listdir(f"{csv_dir_path}/{directory}")
+            for _file in file_list: 
+                if _file[-3:] == "csv":
+                    full_path_list.append(f"{csv_dir_path}/{directory}/{_file}")
+        return full_path_list
