47aeb27b09906fa706fa6a7612f1d9ec1a95ae41
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 16:05:10 +0100

    Update DB_Class.py

--- DB_Class.py
+++ DB_Class.py
@@ -49,13 +49,21 @@ class MongoDBClient:
             print(f"{collection_name} ERROR: {e}")
 
     def query(self, query_filter: list, collection_name: str, calculation: bool, sum_col: str):
+        ###
+        # This function has 2 running modes, these are set by changing calculation to either True 
+        # or False.
+        # If True then it will return the sum of a given coloumn.
+        # If False then it will return a dataframe with the rows from your query.
+        ###
+
         try:
             combined_filter = {'$and': query_filter}
             result = self.db[collection_name].find(combined_filter)
+            df = pd.DataFrame(list(result))
 
             if calculation:
-                print(result[sum_col].sum())
+                return df[sum_col].sum()
 
-            return pd.DataFrame(list(result))
+            return df
         except Exception as e:
             print(f'Error: {e}')  
