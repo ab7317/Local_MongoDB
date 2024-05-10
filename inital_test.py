2b29bc2f74b84cafd8397b2bd6940ab0e126228f
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 08:51:10 +0100

    Update inital_test.py

--- inital_test.py
+++ inital_test.py
@@ -1,44 +1,16 @@
 import pymongo
 import pandas as pd
 
-csv = pd.read_csv("C:/Users/Administrator/Code/rusty_snapshots/snapshots/2024-05-09_17-00/CoinM.csv").to_dict(orient='records')
+class MongoDBClient:
+    def __init__(self, uri, db_name):
 
+        self.client = pymongo.MongoClient(uri)
+        self.db = self.client[db_name]
 
+    def get_collections(self):
+        collection_names = self.db.list_collection_names()
+        #print(collection_names)
+        return collection_names
 
-mongo_uri = "mongodb://localhost:27017/"
-
-client = pymongo.MongoClient(mongo_uri)
-db = client["test_csv"]
-
-collection_name = "2024-05-09_17-00_CoinM"
-collection = db[collection_name]
-
-for i in csv:
-    result = collection.insert_one(i)  # Example document (key-value pair)
-
-# Check if the collection was created successfully
-if result.acknowledged:
-    print(f"Collection '{collection_name}' created successfully!")
-else:
-    print(f"Failed to create collection '{collection_name}'")
-
-print(db.list_collection_names())
-
-try:
-    # Connect to MongoDB
-    client = pymongo.MongoClient(mongo_uri)
-    db = client["test_csv"]
-
-    # Access the specified collection
-    collection = db[collection_name]
-
-    # Retrieve all documents (values) in the collection
-    documents = collection.find()
-
-    # Print the values of each document
-    print(f"Values in '{collection_name}':")
-    for doc in documents:
-        print(doc)
-
-except pymongo.errors.ConnectionFailure:
-    print("Failed to connect to MongoDB.")
+Client = MongoDBClient("mongodb://localhost:27017/", "test_csv")
+Client.get_collections()
