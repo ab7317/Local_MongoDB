ecefa6dd1545f488f0d30e7e4d68d601c3f3f90c
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 11:05:52 +0100

    Update DB_Class.py

--- DB_Class.py
+++ DB_Class.py
@@ -10,14 +10,14 @@ class MongoDBClient:
     def get_collections(self):
         return self.db.list_collection_names()
     
-    def get_collection_content(self, collection_name):
+    def get_collection_content(self, collection_name:str):
         return list(self.db[collection_name].find())
     
-    def insert_row_collection(self, collection_name, row_dict):
+    def insert_row_collection(self, collection_name: str, row_dict: dict):
         self.db[collection_name].insert_one(row_dict)
         print("Row added to collection!")
 
-    def insert_csv_collection(self, collection_name, csv_path):
+    def insert_csv_collection(self, collection_name: str, csv_path: str):
         try:
             csv_dict = pd.read_csv(csv_path).to_dict(orient="records")
             for csv in csv_dict:
@@ -26,13 +26,13 @@ class MongoDBClient:
         except:
             print(f"{collection_name}")
 
-    def query_collection_cols(self, collection_name, cols_to_query):
+    def query_collection_cols(self, collection_name: str, cols_to_query: list):
         print(list(self.db[collection_name].find(
         {field: {"$exists": True} for field in cols_to_query},
         {field: 1 for field in cols_to_query + ["_id"]}
         )))
 
-    def build_csv_path_list(self, csv_dir_path):#
+    def build_csv_path_list(self, csv_dir_path: str):
         ###
         #This function will build a list only of csv files it will ignore any
         #other file types inside the directories
@@ -48,7 +48,7 @@ class MongoDBClient:
                     full_path_list.append(f"{csv_dir_path}/{directory}/{_file}")
         return full_path_list
     
-    def insert_csv_dir_collection(self, csv_full_path_list):
+    def insert_csv_dir_collection(self, csv_full_path_list: list):
         for csv_path in csv_full_path_list:
             collection_name = f"{csv_path.split('/')[-2]}_{csv_path.split('/')[-1]}"
             self.insert_csv_collection(collection_name, csv_path)
