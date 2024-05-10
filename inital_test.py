4cb4a8830dff1c55603da503d09e4c144267aa80
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 09:32:40 +0100

    Update inital_test.py

--- inital_test.py
+++ inital_test.py
@@ -21,9 +21,17 @@ class MongoDBClient:
         csv_dict = pd.read_csv(csv_path).to_dict(orient="records")
         for csv in csv_dict:
            self.db[collection_name].insert_one(csv) 
+        print("CSV added!")
+
+    def query_collection_cols(self, collection_name, cols_to_query):
+        print(list(self.db[collection_name].find(
+        {field: {"$exists": True} for field in cols_to_query},
+        {field: 1 for field in cols_to_query + ["_id"]}
+        )))
 
 
 Client = MongoDBClient("mongodb://localhost:27017/", "test_csv")
-print(Client.get_collections())
+Client.query_collection_cols("2024-05-09_17-00_CoinM_2", ["Exchange", "LiqRisk"])
+#print(Client.get_collections())
 #Client.insert_csv_collection("2024-05-09_17-00_CoinM_2", "C:/Users/Administrator/Code/rusty_snapshots/snapshots/2024-05-09_17-00/CoinM.csv")
-#print(Client.get_collection_content("2024-05-09_17-00_CoinM"))
+#print(Client.get_collection_content("2024-05-09_17-00_CoinM_2"))
