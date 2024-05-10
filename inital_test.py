04dba73d9858f4702a92496b2edd34bb5b668b92
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 09:18:27 +0100

    Update inital_test.py

--- inital_test.py
+++ inital_test.py
@@ -13,8 +13,17 @@ class MongoDBClient:
     def get_collection_content(self, collection_name):
         return list(self.db[collection_name].find())
     
-    def insert_row_collection(self, )
+    def insert_row_collection(self, collection_name, row_dict):
+        self.db[collection_name].insert_one(row_dict)
+        print("Row added to collection!")
+
+    def insert_csv_collection(self, collection_name, csv_path):
+        csv_dict = pd.read_csv(csv_path).to_dict(orient="records")
+        for csv in csv_dict:
+           self.db[collection_name].insert_one(csv) 
 
 
 Client = MongoDBClient("mongodb://localhost:27017/", "test_csv")
-print(Client.get_collection_content("2024-05-09_17-00_CoinM"))
+print(Client.get_collections())
+#Client.insert_csv_collection("2024-05-09_17-00_CoinM_2", "C:/Users/Administrator/Code/rusty_snapshots/snapshots/2024-05-09_17-00/CoinM.csv")
+#print(Client.get_collection_content("2024-05-09_17-00_CoinM"))
