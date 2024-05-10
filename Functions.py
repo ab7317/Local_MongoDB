4a1de392d0bb4b70e3c2f6f41778c8dad568d3f9
ab7317 <137265095+ab7317@users.noreply.github.com>
2024-05-10 14:01:05 +0100

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import sys
+
+db_class_path = "C:/Users/Administrator/Code/MongoDB"
+
+sys.path.append(db_class_path)
+import mongo_class as mc
+
+Client = mc.MongoDBClient("mongodb://localhost:27017/", "test_new")
+
+#print(Client.get_collections())
+
+print(Client.get_collection_content("CoinM"), "\n##########################")
+
+Client.query("Time", "17-00", "CoinM")
+
+csv_files = Client.build_csv_path_list("C:/Users/Administrator/Code/rusty_snapshots/snapshots")
+
+for csv in csv_files:
+    Client.insert_csv_collection(csv.split("/")[-1][:-4], csv)
+
